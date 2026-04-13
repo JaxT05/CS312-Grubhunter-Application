0ca073d90b67e6bb71d87ceeaabf42308ab581eb
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2026-04-13 00:06:08 -0400

    add authentication to graphql mutation resolvers

--- code/grubhunter/graphql/locations/mutations.ts
+++ code/grubhunter/graphql/locations/mutations.ts
@@ -1,14 +1,28 @@
 import { updateWishlist } from "@/mongoose/locations/services";
+import { authGuard } from "../../middleware/auth-guards";
+import { JWT } from "next-auth/jwt";
+
+interface contextInterface {
+    tokenAuth: JWT;
+}
 
 interface UpdateWishlistParams {
     user_id: string;
     location_id: string;
 }
 export const mutationResolvers = {
-    addWishlist: async (_: unknown, param: UpdateWishlistParams, context:[]) => {
+    addWishlist: async (_: unknown, param: UpdateWishlistParams, context: contextInterface) => {
+        const guard = authGuard({userId: param.user_id, locationId: param.location_id}, {authToken: context.tokenAuth});
+        if (!guard || guard instanceof Error) {
+            return guard;
+        } 
         updateWishlist(param.location_id, param.user_id, "add");
         },
-    removeWishlist: async (_: unknown, param: UpdateWishlistParams, context:[]) => {
+    removeWishlist: async (_: unknown, param: UpdateWishlistParams, context: contextInterface) => {
+        const guard = authGuard({userId: param.user_id, locationId: param.location_id}, {authToken: context.tokenAuth});
+        if (!guard || guard instanceof Error) {
+            return guard;
+        } 
         updateWishlist(param.location_id, param.user_id, "remove");
    }
 }
